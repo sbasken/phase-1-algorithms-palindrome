85a152a5a86485e1db21a2d10de8b399b186aa8d
Saki <sbasken@gmail.com>
2023-02-01 17:56:05 -0800

    add isPalindrome function

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedWord = "";
+  for (let i = (word.length - 1); i > -1 ; i-- ) {
+    reversedWord += word[i]
+  }
+  if (reversedWord === word) {
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  - create a function that takes in a word as a parameter
+  - create a empty string
+  - iterate through the word letter by letter
+  - add each letter to the end of the string
+  - compare the string and the parameter
+  - if they match, return true
+  - if not, return false
 */
+
 
 /*
   Add written explanation of your solution here
